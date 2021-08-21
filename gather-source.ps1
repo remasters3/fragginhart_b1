78da6a8893b36f692b6ef76f97cda7ce9fa3a3f9
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2021-08-22 00:23:39 +0100

    08/22/2021 00:23:39

--- gather-source.ps1
+++ gather-source.ps1
@@ -1,10 +1,8 @@
-$etmain="F:\games\WolfET\etmain\"
-$repo="F:\GtkRadiant-1.6.6-20180422\etdev\git\fragginhart_b1\"
-$filelist = get-content "$repo\file.list"
-foreach ($file in $filelist){
-    copy-item -Force "$etmain\$file" "$repo\$file"
+function gather-source {
+    $etmain="F:\games\WolfET\etmain\"
+    $repo="F:\GtkRadiant-1.6.6-20180422\etdev\git\fragginhart_b1\"
+    $filelist = get-content "$repo\file.list"
+    foreach ($file in $filelist){
+        copy-item -Force "$etmain\$file" "$repo\$file"
+    }
 }
-cd $repo
-git add *
-git commit -m $(get-date)
-git push
