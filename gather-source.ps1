e6671732b54b8de312f221ae65261462a6fc3713
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2021-09-05 11:17:28 +0100

    09/05/2021 11:17:28

--- gather-source.ps1
+++ gather-source.ps1
@@ -2,11 +2,16 @@ function gather-source {
     $etmain="F:\games\WolfET\etmain\"
     $repo="F:\GtkRadiant-1.6.6-20180422\etdev\git\fragginhart_b1\"
     $filelist = get-content "$repo\file.list"
-
+    Remove-Item -Path "$repo\maps\fragginhart_b1" -Recurse -force
+    Copy-Item -Path "$etmain\maps\fragginhart_b1" -Destination "$repo\maps\fragginhart_b1" -Recurse
     foreach ($file in $filelist){
         if ($(Test-Path -Path "$localrepo\$file")){
-            Remove-Item -Path "$repo\$file"
+
+                Remove-Item -Path "$repo\$file" -Recurse -force
+            }
+        if ($(Test-Path -Path "$etmain\$file")){
+            Copy-Item -Path "$etmain\$file" -Destination "$repo\$file" -Recurse
         }
-        Copy-Item -Path "$etmain\$file" -Destination "$repo\$file" -Recurse
     }
 }
+#gather-source
